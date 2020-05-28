b6ef57c2f93a734e388126b2aa771df6269fe48d
boyapper <65236566+boyapper@users.noreply.github.com>
2020-05-28 04:10:20 -0700

    Update kidlatallin.sh

--- kidlatallin.sh
+++ kidlatallin.sh
@@ -791,7 +791,7 @@ install
 /bin/cat <<"EOM" >/etc/openvpn/script/login.sh
 #!/bin/bash
 . /etc/openvpn/script/config.sh
-user_name=`mysql -u $USER -p$PASS -h $HOST $DB -sN -e "SELECT user_name FROM users WHERE user_name='$username' AND user_pass='$password' AND is_validated=1 AND frozen=0 AND premium_duration > 0"`
+user_name=`mysql -u $USER -p$PASS -h $HOST $DB -sN -e "SELECT user_name FROM users WHERE user_name='$username' AND user_pass='$password' AND is_validated=1 AND frozen=0 AND duration > 0"`
 [ "$user_name" != '' ] && [ "$user_name" = "$username" ] && echo "user : $username" && echo 'authentication ok.' && exit 0 || echo 'authentication failed.'; exit 1
 
 
