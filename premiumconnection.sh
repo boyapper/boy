85be4f1cebad50b8f2acda061fff207acb783a8c
boyapper <65236566+boyapper@users.noreply.github.com>
2020-07-10 21:21:47 -0700

    Update premiumconnection.sh

--- premiumconnection.sh
+++ premiumconnection.sh
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = 'mysql1.blazingfast.io';
-$DB_user = 'cybertea_kidlat';
+$DB_host = 'webhosting2050.is.cc';
+$DB_user = 'teamkidl_kidlat';
 $DB_pass = 'jan022011';
-$DB_name = 'cybertea_kidlat';
+$DB_name = 'teamkidl_kidlat';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
